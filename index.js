52a7cc719c7521393985eb98cc924380be8f4c57
Karthik Texnidez <texnidez3d@gmail.com>
2024-12-15 12:43:32 +0530

    initialcommit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const schema = require("./model/model")
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://texnidez3d:7GabYvrrwOHDdOxH@datacollectiondatabase.g5ucs.mongodb.net/?retryWrites=true&w=majority&appName=datacollectiondatabase")
+.then(console.log("DB Mongodb Connected Successfully"))
+.catch(error => console.log(error))
+
+app.post('/entry', (req,res)=>
+{
+   schema.create(req.body)
+   .then(result => res.json(result))
+   .catch(err => res.json(err))
+})
+
+app.listen(3000,()=>
+{
+    console.log("Connected to Server")
+})
+
